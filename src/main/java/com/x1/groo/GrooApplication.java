09824377929de0b8c6ccd7bc206799ce1bdb727e
JK-LEE98 <abc9541@gmail.com>
2025-04-24 14:53:29 +0900

    groo 프로젝트 생성

---
+++ src/main/java/com/x1/groo/GrooApplication.java
@@ -0,0 +1,13 @@
+package com.x1.groo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrooApplication.class, args);
+    }
+
+}
