484f2e2e214c655d12ef7c38b2fc1765cafdd3f7
JK-LEE98 <abc9541@gmail.com>
2025-04-24 15:41:24 +0900

    서버 테스트용 Controller 작성 완료

---
+++ src/main/java/com/x1/groo/controller/GrooController.java
@@ -0,0 +1,13 @@
+package com.x1.groo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GrooController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "I'm OK";
+    }
+}
