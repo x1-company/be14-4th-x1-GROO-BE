a30bd53d2ddb046c5619cc0fbbdb453f2b0366a4
cxzaqq <roy1228@naver.com>
2025-04-27 03:18:22 +0900

    🌟 feature: rds 연결 및 상태 체크

--- src/main/java/com/x1/groo/s3/controller/S3Controller.java
+++ src/main/java/com/x1/groo/s3/controller/S3Controller.java
@@ -3,6 +3,7 @@ package com.x1.groo.s3.controller;
 import com.x1.groo.s3.service.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class S3Controller {
 
     private final S3Service s3Service;
+    private final JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public S3Controller(S3Service s3Service) {
+    public S3Controller(S3Service s3Service, JdbcTemplate jdbcTemplate) {
         this.s3Service = s3Service;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @GetMapping("/health2")
@@ -30,5 +33,10 @@ public class S3Controller {
         return ResponseEntity.ok(presignedUrl);
     }
 
+    @GetMapping("/db-check")
+    public ResponseEntity<String> dbCheck() {
+        String result = jdbcTemplate.queryForObject("SELECT category FROM category LIMIT 1", String.class);
+        return ResponseEntity.ok("DB 연결 성공! 가져온 데이터: " + result);
+    }
 
 }
