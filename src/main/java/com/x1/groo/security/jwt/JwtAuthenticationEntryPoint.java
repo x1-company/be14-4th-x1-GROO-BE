0d0003b64fb0eaa8c7b1cdb387791391927d2e87
cxzaqq <roy1228@naver.com>
2025-04-27 03:25:09 +0900

    🔨 fix: 로그 추가

--- src/main/java/com/x1/groo/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/x1/groo/security/jwt/JwtAuthenticationEntryPoint.java
@@ -4,11 +4,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
@@ -16,6 +18,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException)
             throws IOException {
+        log.error(authException.getMessage(), authException);
         if (authException instanceof InsufficientAuthenticationException) {
             // 인증에 부족한 부분이 있을 경우
             response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied");
