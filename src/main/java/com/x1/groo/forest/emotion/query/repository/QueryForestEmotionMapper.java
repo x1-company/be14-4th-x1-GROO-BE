69c33a53351a3873e8e5a037435de511120603b1
mmmv41 <tlsalsrud5068@daum.net>
2025-04-30 14:15:55 +0900

    🌟 feat: 감정의 숲 기록의 조각 조회에 forestId 추가

--- src/main/java/com/x1/groo/forest/emotion/query/repository/QueryForestEmotionMapper.java
+++ src/main/java/com/x1/groo/forest/emotion/query/repository/QueryForestEmotionMapper.java
@@ -4,23 +4,27 @@ import com.x1.groo.forest.emotion.query.dto.QueryForestEmotionDetailDTO;
 import com.x1.groo.forest.emotion.query.dto.QueryForestEmotionMailboxDTO;
 import com.x1.groo.forest.emotion.query.dto.QueryForestEmotionMailboxListDTO;
 import com.x1.groo.forest.emotion.query.dto.QueryForestEmotionUserItemDTO;
-import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface QueryForestEmotionMapper {
 
+    int findUserIdByForestId(@Param("forestId") int forestId);
+
     // 사용자가 보유한 기억의 조각 카테고리별 조회
     List<QueryForestEmotionUserItemDTO> findPieceOfMemory(
             @Param("userId") int userId,
-            @Param("categoryId") int categoryId
-            );
+            @Param("categoryId") int categoryId,
+            @Param("forestId") int forestId
+    );
 
     List<QueryForestEmotionMailboxListDTO> findMailboxList(
             @Param("userId") int userId,
             @Param("forestId") int forestId
-            );
+    );
 
     List<QueryForestEmotionMailboxDTO> findMailboxDetail(
             @Param("userId") int userId,
--- src/main/java/com/x1/groo/forest/emotion/query/service/QueryForestEmotionServiceImpl.java
+++ src/main/java/com/x1/groo/forest/emotion/query/service/QueryForestEmotionServiceImpl.java
@@ -5,9 +5,11 @@ import com.x1.groo.forest.emotion.query.dto.QueryForestEmotionMailboxDTO;
 import com.x1.groo.forest.emotion.query.dto.QueryForestEmotionMailboxListDTO;
 import com.x1.groo.forest.emotion.query.dto.QueryForestEmotionUserItemDTO;
 import com.x1.groo.forest.emotion.query.repository.QueryForestEmotionMapper;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QueryForestEmotionServiceImpl implements QueryForestEmotionService {
@@ -16,15 +18,21 @@ public class QueryForestEmotionServiceImpl implements QueryForestEmotionService 
     private QueryForestEmotionMapper queryForestEmotionMapper;
 
     // 사용자가 보유한 기억의 조각 카테고리별 조회
-    public List<QueryForestEmotionUserItemDTO> getPieceOfMemory (int userId, int categoryId) {
-        return queryForestEmotionMapper.findPieceOfMemory(userId, categoryId);
+    public List<QueryForestEmotionUserItemDTO> getPieceOfMemory(int userId, int categoryId, int forestId) {
+        int forestOwnerId = queryForestEmotionMapper.findUserIdByForestId(forestId);
+
+        if (forestOwnerId != userId) {
+            throw new AccessDeniedException("해당 유저는 forest에 접근할 수 없습니다.");
+        }
+
+        return queryForestEmotionMapper.findPieceOfMemory(userId, categoryId, forestId);
     }
 
-    public List<QueryForestEmotionMailboxListDTO> getMailboxList (int userId, int forestId) {
+    public List<QueryForestEmotionMailboxListDTO> getMailboxList(int userId, int forestId) {
         return queryForestEmotionMapper.findMailboxList(userId, forestId);
     }
 
-    public List<QueryForestEmotionMailboxDTO> getMailboxDetail (int userId, int id) {
+    public List<QueryForestEmotionMailboxDTO> getMailboxDetail(int userId, int id) {
         return queryForestEmotionMapper.findMailboxDetail(userId, id);
     }
 
